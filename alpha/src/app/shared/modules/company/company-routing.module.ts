28df3fc5527d4b1695fbe85a8074e40e0282d43d
Giga <60932604+Gkhundadze@users.noreply.github.com>
2021-02-18 16:57:46 +0400

    footer styling
    styles and routerlins in footer

--- alpha/src/app/shared/modules/company/company-routing.module.ts
+++ alpha/src/app/shared/modules/company/company-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './company/about/about.component';
 import { CompanyComponent } from './company/company.component';
+import { ContactComponent } from './company/contact/contact.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CompanyComponent,
+  },
+  {
+    path: 'about-us',
+    component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
   }
 ];
 
--- alpha/src/app/shared/modules/company/company.module.ts
+++ alpha/src/app/shared/modules/company/company.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CompanyRoutingModule } from './company-routing.module';
 import { CompanyComponent } from './company/company.component';
+import { AboutComponent } from './company/about/about.component';
+import { ContactComponent } from './company/contact/contact.component';
 
 
 @NgModule({
-  declarations: [CompanyComponent],
+  declarations: [CompanyComponent, AboutComponent, ContactComponent],
   imports: [
     CommonModule,
     CompanyRoutingModule
